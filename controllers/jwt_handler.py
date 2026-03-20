26aa6036e2ff07c9c4f44401c6e03af220615919
veil95 <lorplol95@gmail.com>
2026-03-20 17:59:42 +0300

    добавил че та типа httponly cookie хз надесь это то ваще

--- controllers/jwt_handler.py
+++ controllers/jwt_handler.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from fastapi import HTTPException
 from os import getenv
 from dotenv import load_dotenv
 from jose import jwt
@@ -10,12 +11,14 @@ ALGORITHM = getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 REFRESH_TOKEN_EXPIRE_DAYS = int(getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
 
+
 def create_refresh_token(username: str) -> str:
     data = {"sub": username}
     expire = datetime.utcnow() + timedelta(days = REFRESH_TOKEN_EXPIRE_DAYS)
     data.update({"exp": expire,
                  "type": "refresh_token"})
     return jwt.encode(data, SECRET_KEY, algorithm = ALGORITHM)
+
 
 def create_access_token(username: str) -> str:
     data = {"sub": username}
@@ -24,9 +27,13 @@ def create_access_token(username: str) -> str:
                  "type": "access_token"})
     return jwt.encode(data, SECRET_KEY, algorithm = ALGORITHM)
 
+
 def decode_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms = ALGORITHM)
         return payload
     except jwt.ExpiredSignatureError:
-        return None
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.JWTError:
+        raise HTTPException(status_code=403, detail="invalid token")
+
--- views/auth.py
+++ views/auth.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Response
 from controllers.user_auth import AuthController
 from controllers.check_rate_limit import Ratelimit
-from controllers.jwt_handler import create_access_token
+from controllers.jwt_handler import create_access_token, create_refresh_token, decode_token
 from model.user import UserLogin, UserRequestRegistation
 from model.user import user_instance as user
 
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/auth", tags=["authentication"])
 
 
 @router.post("/login")
-async def login(user_data: UserLogin):
+async def login(user_data: UserLogin, response: Response):
 
     ratelimit.increment_login_attempt(user_data.username)
 
@@ -30,6 +30,15 @@ async def login(user_data: UserLogin):
         )
 
     access_token = create_access_token(user_data.username)
+
+    refresh_token = create_refresh_token(user.username)
+
+    response.set_cookie(
+        key="refresh_token",
+        value=refresh_token,
+        httponly=True,
+        secure=True,
+    )
 
     ratelimit.reset_attempts(user_data.username)
 
